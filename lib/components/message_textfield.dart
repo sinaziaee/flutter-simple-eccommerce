518af65c53170343d15e7bda3ea841656c22aeab
sinaziaee <sinaziaee99@gmail.com>
2021-04-15 16:46:07 +0430

    finished chatScreen with one bug

--- lib/components/message_textfield.dart
+++ lib/components/message_textfield.dart
@@ -11,47 +11,64 @@ class CustomMessageTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          height: 50,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10),
-              topRight: Radius.circular(10),
-            ),
-          ),
-          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          child: TextField(
-            cursorColor: Colors.black,
-            controller: controller,
-            decoration: InputDecoration(
-              labelText: 'Enter your messages...',
-              contentPadding: EdgeInsets.only(
-                left: 10,
-                right: 10,
-                bottom: 20,
-              ),
-              border: kOutLineInputBorder.copyWith(
-                borderSide: BorderSide(
-                  color: Colors.grey,
-                  width: 0.5,
-                ),
-              ),
-              focusedBorder: kOutLineInputBorder.copyWith(
-                borderSide: BorderSide(
-                  color: Colors.black,
-                  width: 1.5,
-                ),
-              ),
-            ),
+    return Container(
+      height: 53,
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(15),
+          topRight: Radius.circular(15),
+        ),
+      ),
+      padding: EdgeInsets.only(top: 1),
+      child: Container(
+        height: 51,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(15),
+            topRight: Radius.circular(15),
           ),
         ),
-        IconButton(
-          icon: Icon(Icons.send),
-          onPressed: onSendPressed,
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 50,
+                child: TextField(
+                  cursorColor: Colors.black,
+                  controller: controller,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your messages...',
+                    contentPadding: EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      bottom: 20,
+                    ),
+                    border: kOutLineInputBorder.copyWith(
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                        width: 0.5,
+                      ),
+                    ),
+                    focusedBorder: kOutLineInputBorder.copyWith(
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                        width: 1.5,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.send),
+              onPressed: onSendPressed,
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce/components/message_textfield.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../constants.dart';
 import '../static_methods.dart';
@@ -13,6 +16,8 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   TextEditingController controller = TextEditingController();
+  FirebaseFirestore fireStore = FirebaseFirestore.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
 
   // FocusNode node;
 
@@ -35,7 +40,13 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget bodyBuilder() {
     return Column(
       children: [
+        SizedBox(
+          height: 10,
+        ),
         chatBuilder(),
+        SizedBox(
+          height: 10,
+        ),
         CustomMessageTextField(
           controller: controller,
           onSendPressed: () {
@@ -47,7 +58,95 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
   Widget chatBuilder() {
-    return SizedBox();
+    return Expanded(
+      child: StreamBuilder<QuerySnapshot>(
+        stream: fireStore.collection('messages').orderBy('dateTime', descending: true).snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          // print(snapshot.data.docs.first.data());
+          if (snapshot.hasData) {
+            // print('-------------------------------------------');
+            final messages = snapshot.data.docs;
+            return ListView.builder(
+              reverse: true,
+              itemBuilder: (context, index) {
+                return messageContainer(messages[index].data());
+              },
+              itemCount: messages.length,
+            );
+          }
+          return Center(
+            child: kCustomProgressIndicator,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget messageContainer(Map map) {
+    bool isMe = auth.currentUser.email == map['sender'];
+    return Container(
+      margin: EdgeInsets.only(
+        top: 10,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment:
+            isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: [
+              if(!isMe)...[
+                Padding(
+                  padding: EdgeInsets.only(left: 5, bottom: 5),
+                  child: Text(
+                    map['sender'],
+                    style: TextStyle(
+                      fontSize: 11
+                    ),
+                  ),
+                ),
+              ],
+              Container(
+                margin: EdgeInsets.only(
+                  left: isMe ? 30 : 10,
+                  right: !isMe ? 30: 10,
+                ),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 5,
+                ),
+                decoration: BoxDecoration(
+                  color: isMe ? Colors.lightBlue[100] : Colors.grey[300],
+                  borderRadius: BorderRadius.only(
+                    topLeft: isMe ? Radius.circular(20) : Radius.circular(5),
+                    bottomLeft: isMe ? Radius.circular(5) : Radius.circular(5),
+                    topRight: !isMe ? Radius.circular(20) : Radius.circular(5),
+                    bottomRight: !isMe ? Radius.circular(5) : Radius.circular(5),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text(
+                        map['messageBody'],
+                      ),
+                    ),
+                    Text(
+                      '${DateTime.parse(map['dateTime'].toDate().toString()).toString().substring(11,16)}',
+                      style: TextStyle(
+                          fontSize: 10
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
   onSendPressed() {
@@ -59,5 +158,11 @@ class _ChatScreenState extends State<ChatScreen> {
     uploadMessage(text);
   }
 
-  uploadMessage(String text) {}
+  uploadMessage(String text) {
+    fireStore.collection('messages').add({
+      'sender': auth.currentUser.email,
+      'messageBody': text,
+      'dateTime': DateTime.now(),
+    });
+  }
 }
